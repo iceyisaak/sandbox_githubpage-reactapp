c216e66800aa181fa321d88f449d0585c3356f9c
Icey Isaak <me@iceyisaak.com>
2020-05-21 18:44:37 +0200

    Updates

---
+++ precache-manifest.72e753604f3465150e552bb76305ca55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7ed2f61f0d94c1ae73a7c3877a050c",
+    "url": "/sandbox_githubpage-reactapp/index.html"
+  },
+  {
+    "revision": "9db279c6b644adf7d214",
+    "url": "/sandbox_githubpage-reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81112826c729696fa2c4",
+    "url": "/sandbox_githubpage-reactapp/static/js/2.2e899634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sandbox_githubpage-reactapp/static/js/2.2e899634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db279c6b644adf7d214",
+    "url": "/sandbox_githubpage-reactapp/static/js/main.0fc40e04.chunk.js"
+  },
+  {
+    "revision": "1ceaaa972a6dd1e06be7",
+    "url": "/sandbox_githubpage-reactapp/static/js/runtime-main.ccafa6f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sandbox_githubpage-reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
